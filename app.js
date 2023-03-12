aa58cb9c7663be9abcb4cfa7209c6d0c767a8f06
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2023-03-12 12:36:46 +0900

    app.jsのリファクタリングをしました。

--- app.js
+++ app.js
@@ -5,49 +5,36 @@ const answers = [
     'Switch',
     'DS'
   ];
+
 const correct = 'DS';
-
-console.log(document.getElementById('js-question').textContent );
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button');
-
-
-let buttonIndex = 0;
 let buttonLength = $button.length;
-while(buttonIndex <buttonLength){
-  $button[buttonIndex].textContent = answers[buttonIndex];
-  buttonIndex++;
+
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  while(buttonIndex <buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
 }
 
-$button[0].addEventListener('click', ()=> {
-  if(correct === $button[0].textContent){
-    window.alert('正解！');
-  }else{
-    window.alert('不正解！');
-  }
-});
+setupQuiz();
 
-$button[1].addEventListener('click', ()=> {
-  if(correct === $button[1].textContent){
-    window.alert('正解！');
-  }else{
-    window.alert('不正解！');
-  }
-});
 
-$button[2].addEventListener('click', ()=> {
-  if(correct === $button[2].textContent){
-    window.alert('正解！');
-  }else{
-    window.alert('不正解！');
-  }
-});
+//ボタンをクリックしたら正誤判定
 
-$button[3].addEventListener('click', ()=> {
-  if(correct === $button[3].textContent){
+const clickHandler = (e) => {
+  if(correct === e.target.textContent){
     window.alert('正解！');
   }else{
     window.alert('不正解！');
   }
-});
+};
+
+for(handlerIndex=0; handlerIndex<buttonLength; handlerIndex++){
+  $button[handlerIndex].addEventListener('click', (e)=> {
+    clickHandler(e);
+  });
+}
